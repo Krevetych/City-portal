2df2a14aab93455f1fb90d2e46fa72233fbb2225
Krevetych <timurbyvalcev95@gmail.com>
2023-12-21 11:34:14 +0400

    Bug Fix

--- client/src/components/RealTimeCount.jsx
+++ client/src/components/RealTimeCount.jsx
@@ -3,22 +3,9 @@ import { ProblemContext } from '../main'
 
 const RealTimeCount = () => {
 	const { problem } = useContext(ProblemContext)
-	const [visible, setVisible] = useState(true)
 	const [count, setCount] = useState(
 		problem?.filter(e => e.status === 'Решена').length
 	)
-
-	const click = () => {
-		if (
-			confirm(
-				'Вы уверены? Вернуть счетчик можно будет только перезагрузкой страницы'
-			)
-		) {
-			setVisible(false)
-		} else {
-			alert('Спасибо, что не стали скрывать счетчик')
-		}
-	}
 
 	useEffect(() => {
 		let time = setInterval(() => {
@@ -32,12 +19,9 @@ const RealTimeCount = () => {
 
 	return (
 		<div
-			className={`${
-				!visible ? 'opacity-0' : 'opacity-100'
-			} fixed z-50 flex gap-2 bottom-[81px] text-sm bg-primary w-fit opacity-20 font-bold right-0 p-3 m-2 rounded-lg duration-500 hover:opacity-100 md:bottom-0 md:text-xl`}
+			className={`fixed z-50 flex gap-2 bottom-[81px] text-sm bg-primary w-fit opacity-20 font-bold right-0 p-3 m-2 rounded-lg duration-500 hover:opacity-100 md:bottom-0 md:text-xl`}
 		>
 			Решенных заявок: {count}
-			<img src='./modal/close.svg' alt='' onClick={click} />
 		</div>
 	)
 }
